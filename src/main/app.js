e3114cd5e18f455d8a59a2ca119890152364cea2
wangsp14 <wangsp14@midea.com>
2022-06-29 17:50:37 +0800

    添加 release 相关的内容

--- src/main/app.js
+++ src/main/app.js
@@ -16,6 +16,7 @@ app.on("ready", () => {
     });
     if (app.isPackaged) {
         console.log("打包后执行");
+        mainWindow.loadURL("https://wwww.hao123.com")
     } else {
        // 开发时 http 访问
         mainWindow.loadURL(`http://localhost:${process.env.WEB_PORT}/`);
