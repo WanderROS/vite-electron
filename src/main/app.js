3d448220b9071f8f183d223402bb73d9b926d250
WanderROS <wsp1194409532@gmail.com>
2022-07-02 10:22:16 +0800

    网络代理

--- src/main/app.js
+++ src/main/app.js
@@ -1,6 +1,7 @@
-import { app, BrowserWindow,protocol} from "electron";
+import { app, BrowserWindow,protocol,session} from "electron";
 import path from "path";
 import fs from "fs";
+const youtubedl = require('youtube-dl-exec')
 
 protocol.registerSchemesAsPrivileged([
     {
@@ -14,7 +15,26 @@ protocol.registerSchemesAsPrivileged([
     },
 ]);
 
+
+// youtubedl('https://www.youtube.com/watch?v=2ueCGvCL27E', {
+//     proxy: 'socks5://127.0.0.1:1080'
+// }).then(output => console.log(output))
+
+
+
+
+
+
 app.on("ready", () => {
+    const subprocess = youtubedl.exec('https://www.youtube.com/watch?v=GNQ43LzDSW8', {
+        dumpJson:true,
+        proxy: 'socks5://127.0.0.1:1080'
+    });
+    subprocess.stdout.on("data", (data) => {
+        const liveData = data.toString();
+        // console.log(JSON.parse(liveData))
+        console.log(liveData)
+    });
     protocol.registerBufferProtocol("app", (request, response) => {
         let pathName = new URL(request.url).pathname;
         let extension = path.extname(pathName).toLowerCase();
@@ -45,7 +65,19 @@ app.on("ready", () => {
             webSecurity: false,
             nodeIntegration: true,
             contextIsolation: false,
-            enableRemoteModule: true
+            enableRemoteModule: true,
+            partition:'persist:unique_random_path2'
+        },
+    });
+  let mainWindow2 = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            webSecurity: false,
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+            partition:'persist:unique_random_path'
         },
     });
     if (app.isPackaged) {
@@ -53,7 +85,27 @@ app.on("ready", () => {
         console.log(`app://./index.html`)
         mainWindow.loadURL(`app://./index.html`);
     } else {
+       // mainWindow.webContents.session.setProxy({
+       //     proxyRules: 'socks5://localhost:1080'
+       // }).then(r =>{
+       //     console.log(r)
+       // })
+        mainWindow.webContents.session.setProxy({
+           proxyRules: 'http://localhost:8889'
+       }).then(r =>{
+          console.log(r)
+       })
+      //  console.log(app.commandLine)
+
        // 开发时 http 访问
-        mainWindow.loadURL(`http://localhost:${process.env.WEB_PORT}/`);
+       //  mainWindow.loadURL(`http://localhost:${process.env.WEB_PORT}/`);
+        mainWindow.loadURL("https://ip38.com/");
+
+        mainWindow2.webContents.session.setProxy({
+            proxyRules: ''
+        }).then(r =>{
+            console.log(r)
+        })
+        mainWindow2.loadURL("https://ip38.com/");
     }
 });
